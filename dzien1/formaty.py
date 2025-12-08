a127b0a497a903b4fe6648ff6ca2ef253b73fe09
radoslawjaniak <rajkokret660@gmail.com>
2025-12-08 14:16:56 +0100

    [1 6] update formaty.py

---
+++ dzien1/formaty.py
@@ -0,0 +1,27 @@
+user = "Tomek"  # str
+wiek = 39  # int
+wersja = 3.90001
+print(type(wersja))  # zmiennoprzecinkowa, <class 'float'>
+print(wersja)  # 3.90001
+
+print("Witaj %s, masz teraz %d lat." % (user, wiek))
+# %s - str
+# %d - digit
+
+# TypeError: %d format: a real number is required, not str
+# sprawdza typy
+# print("Witaj %d, masz teraz %s lat." % (user, wiek))
+
+# f - string
+print(f"Witaj {user}, masz teraz {wiek} lat.")
+# Witaj Tomek, masz teraz 39 lat.
+# %i - int
+# %f - float
+
+print("Używamy wersji pythona %i" % 3)  # Używamy wersji pythona 3
+print("Używamy wersji pythona %f" % 3)  # Używamy wersji pythona 3.000000
+print("Używamy wersji pythona %.2f" % 3.9)  # Używamy wersji pythona 3.90
+print("Używamy wersji pythona %.1f" % 3.9)  # Używamy wersji pythona 3.9
+print("Używamy wersji pythona %.0f" % 3.9)  # Używamy wersji pythona 4 zaokrągla
+print("Używamy wersji pythona %.f" % 3.9)  # Używamy wersji pythona 4 zaokrągla
+
