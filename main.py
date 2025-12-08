6ee78fed2a1dc6f6177a6395614adf7f562d31ab
radoslawjaniak <rajkokret660@gmail.com>
2025-12-08 11:27:32 +0100

    [1 1] init

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# (.venv) radoslawjaniak@MacBook-Air-radosaw-2 PythonProjectPodstawy_8_12_2025 % python main.py
+# Hi, PyCharm
+# 2x shift - wyszukiwarka
+# wheel, theme
