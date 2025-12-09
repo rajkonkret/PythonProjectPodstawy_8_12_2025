0d0a467d3371933c4f42bdc146bdd42cbbbf5039
radoslawjaniak <rajkokret660@gmail.com>
2025-12-09 15:57:02 +0100

    [2 17] update typy_danych_5_slownik.py

--- dzien2/warunki.py
+++ dzien2/warunki.py
@@ -51,3 +51,29 @@ if n > 3:
 if (n := len(a)) > 3:
     print(f"Długosć wynosi: {n}, więcej niż 3")
 
+# podatek = 0
+# zarobki = int(input("Podaj zarobki"))
+#
+# if zarobki < 10_000:
+#     podatek = 0
+# elif zarobki < 40_000:
+#     podatek = 0.2
+# elif zarobki < 100_000:
+#     podatek = 0.4
+# else:
+#     podatek = 0.9
+#
+# print(f"Podatek wynosi: {zarobki * podatek} pln.")
+# # od 10000 do 39999 podatek 0.2
+
+suma_zam = 150
+if suma_zam > 100:
+    rabat = 25
+else:
+    rabat = 0
+
+print(f"Rabat wynosi: {rabat}")  # Rabat wynosi: 25
+
+# operator warunkowy
+rabat = 25 if suma_zam > 100 else 0
+print(f"Rabat wynosi: {rabat}")  # Rabat wynosi: 25
