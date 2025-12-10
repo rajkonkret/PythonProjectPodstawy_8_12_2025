05c122546a64771b9cf1ce1f432c9f405518375c
radoslawjaniak <rajkokret660@gmail.com>
2025-12-10 11:59:07 +0100

    [3 7] update petle_zad1.py

--- dzien3/petle_zad1.py
+++ dzien3/petle_zad1.py
@@ -28,3 +28,41 @@ for _ in range(6):
     lista_wynik.append(wyn)
 
 print(lista_wynik)  # [19, 41, 8, 36, 34, 42]
+
+for i in range(10):
+    if i % 2 == 0:  # modulo, reszta z dzielenia
+        print(i, "parzysta")
+
+# 0 parzysta
+# 2 parzysta
+# 4 parzysta
+# 6 parzysta
+# 8 parzysta
+
+# parzyste do nowej listy
+lista_parzyste = []
+for j in range(10):
+    if j % 2 == 0:
+        lista_parzyste.append(j)
+
+print(lista_parzyste)  # [0, 2, 4, 6, 8]
+
+# list comprehensions
+lista_parzyste = [j for j in range(10) if j % 2 == 0]
+print(lista_parzyste)  # [0, 2, 4, 6, 8]
+
+for i in range(1, 10):
+    print(i)
+
+for c in lista_parzyste:  # dla wszystkich elementów kolekcji
+    print(c)
+
+lista_nazwy = ["Ala", "Tomek", "Zenek", "Basia"]
+
+for p in lista_nazwy:
+    print(p) # kolejne elementy listy
+# Ala
+# Tomek
+# Zenek
+# Basia
+
