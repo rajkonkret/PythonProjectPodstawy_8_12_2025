9f95c03e764a80796b7a34e780891d97afa3f256
radoslawjaniak <rajkokret660@gmail.com>
2025-12-10 16:01:50 +0100

    [3 24] update kalkulator.py

---
+++ dzien3/pliki_zad1.py
@@ -0,0 +1,18 @@
+# filehandler
+# context manager
+# with - context manager w pythonie
+
+# ========= ===============================================================
+#     Character Meaning
+#     --------- ---------------------------------------------------------------
+#     'r'       open for reading (default)
+#     'w'       open for writing, truncating the file first
+#     'x'       create a new file and open it for writing
+#     'a'       open for writing, appending to the end of the file if it exists
+#     'b'       binary mode
+#     't'       text mode (default)
+#     '+'       open a disk file for updating (reading and writing)
+#     ========= ===============================================================
+with open("test.log", "w") as f:
+    f.write("Powitanie\n")
+
