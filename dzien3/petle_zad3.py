14695fcde597080234339ba65bf1aeb3ad17db4b
radoslawjaniak <rajkokret660@gmail.com>
2025-12-10 14:28:07 +0100

    [3 17] update petle_zad3.py

--- dzien3/petle_zad3.py
+++ dzien3/petle_zad3.py
@@ -16,12 +16,25 @@ while licznik < 10:
     licznik += 1
     print("Komunikat 3 !!!")
 
-password = input("Podaj hasło:")
-while password != "secret":  # != różne
-    password = input("Podaj ponownie:")
+# password = input("Podaj hasło:")
+# while password != "secret":  # != różne
+#     password = input("Podaj ponownie:")
 
 # Podaj hasło:asasa
 # Podaj ponownie:asdeda
 # Podaj ponownie:asdasdas
 # Podaj ponownie:zzxcvcz
 # Podaj ponownie:secret
+
+lista = []
+lista_int = []
+
+while True:
+    wej = input("Podaj liczbę: ")
+    if not wej.isnumeric():
+        break
+    lista.append(wej)
+    lista_int.append(int(wej))
+
+print(lista)
+print(lista_int)
