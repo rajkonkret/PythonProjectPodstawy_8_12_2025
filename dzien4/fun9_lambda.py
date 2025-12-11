2e0af8180d48f8b61bd252c80c58d67e820c127c
radoslawjaniak <rajkokret660@gmail.com>
2025-12-11 14:10:43 +0100

    [4 14] update fun9

--- dzien4/fun9_lambda.py
+++ dzien4/fun9_lambda.py
@@ -21,3 +21,51 @@ print(wiek(10))  # nastolatek
 print(wiek(17))  # nastolatek
 print(wiek(18))  # dorosły
 print(wiek(25))  # dorosły
+
+# mapowanie danych
+lista = [1, 2, 14, 24, 50, 67, 80, 100, 200, 500]
+
+l1 = []
+for i in lista:
+    l1.append(i * 2)
+print(l1)
+
+print([i * 2 for i in lista])
+
+
+# [2, 4, 28, 48, 100, 134, 160, 200, 400, 1000]
+
+def zmien(x):
+    return x * 2
+
+
+l2 = []
+for i in lista:
+    l2.append(zmien(i))
+
+# map() - wykonuje na kolejnych danch, zadaną funkcję
+print(f"Zastosowania map():{list(map(zmien, lista))}")
+# Zastosowania map():[2, 4, 28, 48, 100, 134, 160, 200, 400, 1000]
+
+# lambda jako funkcjia anonimowa
+print(f"Zastosowania map():{list(map(lambda x: x * 2, lista))}")
+print(f"Zastosowania map():{list(map(lambda x: x * 4, lista))}")
+print(f"Zastosowania map():{list(map(lambda x: x * 8, lista))}")
+
+# filtrowanie danych
+l4 = []
+for i in lista:
+    if i < 3:
+        l4.append(i)
+print(l4)  # [1, 2]
+
+# filter() - zwraca elementy spełniajce warunek
+print(f"Zastosowannie filter(): {list(filter(lambda x: x < 3, lista))}")
+print(f"Zastosowannie filter(): {list(filter(lambda x: x < 50, lista))}")
+print(f"Zastosowannie filter(): {list(filter(lambda x: x > 30, lista))}")
+print(f"Zastosowannie filter(): {list(filter(lambda x: x > 3 and x < 100, lista))}")
+print(f"Zastosowannie filter(): {list(filter(lambda x: 3 < x < 100, lista))}")
+# Zastosowannie filter(): [14, 24, 50, 67, 80]
+# Zastosowannie filter(): [14, 24, 50, 67, 80]
+# Zastosowannie filter(): [1, 2]
+# Zastosowannie filter(): [1, 2]
