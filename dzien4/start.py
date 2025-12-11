4f036f71c94f29a798672e715a1c0a4fc792cabc
radoslawjaniak <rajkokret660@gmail.com>
2025-12-11 14:56:59 +0100

    [4 18] update pakiet

--- dzien4/start.py
+++ dzien4/start.py
@@ -1,3 +1,17 @@
 import fun8
 
 fun8.all_params(1, 2, 3, 4)
+
+import pakiet
+# pakiet.powianie()
+
+from pakiet import fun
+fun.powitanie() # Hello
+
+# alias
+import pakiet.fun as pk
+pk.powitanie()
+# Hello
+
+# po dodaniu w __all__
+pakiet.info()
---
+++ pakiet/fun.py
@@ -0,0 +1,10 @@
+def powitanie():
+    print("Hello")
+
+
+def info():
+    print("Jestem pakietem v. 1.1")
+
+
+if __name__ == '__main__':
+    info()
