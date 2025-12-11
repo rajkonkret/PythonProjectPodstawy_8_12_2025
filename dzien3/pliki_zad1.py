58b1d9ff8728b6ca0b808ed09a475b10eae1a264
radoslawjaniak <rajkokret660@gmail.com>
2025-12-11 09:19:47 +0100

    [4 1] update pliki_zad1.py

--- dzien3/pliki_zad1.py
+++ dzien3/pliki_zad1.py
@@ -13,6 +13,32 @@
 #     't'       text mode (default)
 #     '+'       open a disk file for updating (reading and writing)
 #     ========= ===============================================================
-with open("test.log", "w") as f:
+with open("test.log", "w", encoding='utf-8') as f:
     f.write("Powitanie\n")
 
+# f.write("l") # ValueError: I/O operation on closed file.
+
+# x - gdy plik istnieje mamy odpowiedni komunikat
+# FileExistsError: [Errno 17] File exists: 'test.log'
+# with open("test.log", "x") as f:
+#     f.write("Powitanie\n")
+
+with open("test.log", "w",  encoding='utf-8') as f:
+    f.write("Powitanie2\n")
+
+# a - dodanie do istniejącego pliku
+with open("test.log", "a",  encoding='utf-8') as f:
+    f.write("Dodane\n")
+    f.write("Dodane2\n")
+    f.write("Dodane23\n")
+    f.write("Dodśane23\n")
+
+# r - odczyt
+with open("test.log", "r",  encoding='utf-8') as f:
+    lines = f.read()
+
+print(lines)
+# Powitanie2
+# Dodane
+# Dodane2
+# Dodane23
