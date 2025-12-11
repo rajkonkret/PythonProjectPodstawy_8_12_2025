554cd09b63d9fd28c1c7727217360228ab1ac09b
radoslawjaniak <rajkokret660@gmail.com>
2025-12-11 14:44:25 +0100

    [4 17] update start.py

--- dzien4/fun8.py
+++ dzien4/fun8.py
@@ -3,17 +3,7 @@ def all_params(a, b, /, c=42, d=256):
     print(f"{c=}, {d=}")
 
 
-all_params(1, 2)
-all_params(1, 2, 3, 4)
 
-
-# TypeError: all_params() got some
-# positional-only arguments passed as keyword arguments: 'a, b'
-# all_params(a=1, b=2)
-# a=1, b=2
-# c=42, d=256
-# / rozdiela mozliwosc uzycia argumentow
-# po lewej stronie tylko po pozycji
 
 def all_params2(name, b, /, c=42, *args, d=67, **kwargs):
     print(f"{name=}")
@@ -23,11 +13,23 @@ def all_params2(name, b, /, c=42, *args, d=67, **kwargs):
     print(f"{args=}")
     print(f"{kwargs=}")
 
+if __name__ == '__main__':
 
-all_params2(1, 2)
-all_params2("Raeedk", 2, 4)
-all_params2("Raeedk", 2, c=4)
-all_params2("Raeedk", 2, 4, 1, 2, 3, 4, 5, 6, 7, 8)
-all_params2("Raeedk", 2, 4, 1, 2, 3, 4, 5, 6, 7, 8, d=90)
-all_params2("Raeedk", 2, 4, 1, 2, 3, 4, 5, 6, 7, 8, d=90, temat="Topic")
-all_params2("Raeedk", 2, 4, 1, 2, 3, 4, 5, 6, 7, 8, d=90, temat="Topic", b=1)
+    all_params(1, 2)
+    all_params(1, 2, 3, 4)
+
+
+    # TypeError: all_params() got some
+    # positional-only arguments passed as keyword arguments: 'a, b'
+    # all_params(a=1, b=2)
+    # a=1, b=2
+    # c=42, d=256
+    # / rozdiela mozliwosc uzycia argumentow
+    # po lewej stronie tylko po pozycji
+    all_params2(1, 2)
+    all_params2("Raeedk", 2, 4)
+    all_params2("Raeedk", 2, c=4)
+    all_params2("Raeedk", 2, 4, 1, 2, 3, 4, 5, 6, 7, 8)
+    all_params2("Raeedk", 2, 4, 1, 2, 3, 4, 5, 6, 7, 8, d=90)
+    all_params2("Raeedk", 2, 4, 1, 2, 3, 4, 5, 6, 7, 8, d=90, temat="Topic")
+    all_params2("Raeedk", 2, 4, 1, 2, 3, 4, 5, 6, 7, 8, d=90, temat="Topic", b=1)
---
+++ dzien4/start.py
@@ -0,0 +1,3 @@
+import fun8
+
+fun8.all_params(1, 2, 3, 4)
