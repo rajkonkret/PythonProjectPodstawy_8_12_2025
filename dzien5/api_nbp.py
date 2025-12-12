2c9d2bf17ab42aba3b16d308552d6b88bd94ae40
radoslawjaniak <rajkokret660@gmail.com>
2025-12-12 12:36:04 +0100

    [5 12] update api_nb.py

--- dzien5/api_nbp.py
+++ dzien5/api_nbp.py
@@ -1,1 +1,14 @@
+import requests
+
 url = "https://api.nbp.pl/api/exchangerates/rates/A/EUR/"
+
+response = requests.get(url)
+print(response)
+
+data = response.json()
+print(data)
+# {'table': 'A', 'currency': 'euro', 'code': 'EUR',
+# 'rates': [{'no': '241/A/NBP/2025', 'effectiveDate': '2025-12-12', 'mid': 4.2271}]}
+print(data['rates'])
+print(data['rates'][0])
+print(data['rates'][0]['mid'])  # 4.2271
