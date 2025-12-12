99c165f012a69fb281ba228821f3199c7b77321f
radoslawjaniak <rajkokret660@gmail.com>
2025-12-12 13:45:53 +0100

    [5 15] update baza2.py

---
+++ dzien5/baza2.py
@@ -0,0 +1,35 @@
+# baza danych - system prechowywania danych
+# silnik - mechanizm zarządzania danymi w bazie
+# relacyjne, nierelacyjne
+# sql - mysql, posgress, oragle, firebird, mssql, sqlite
+# nosql = MogoDB
+
+import sqlite3
+
+try:
+    conn = sqlite3.connect('baza_danych.db')
+    c = conn.cursor()
+    print("Baza danych została podłączona")
+
+    query = """
+    CREATE TABLE IF NOT EXISTS developers(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    salary REAL NOT NULL);
+    """
+    c.execute(query)
+    conn.commit()
+
+    insert = """
+    INSERT INTO developers (id, name, email, salary)
+    VALUES (1, 'Radek','raj@raj.pl', 10000);
+    """
+    c.execute(insert)
+    conn.commit()
+except Exception as e:
+    print("Bład podłączennia bazy danych:", e)
+finally:
+    if conn:
+        conn.close()
+        print("Połączenie zostało zamknięte")
